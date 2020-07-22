a29c647d35dfbcfc332daae09c591696e63f811f
Annalise Murphy <nali247@gmail.com>
2020-07-21 22:12:51 -0700

    images should be rendering. Currenly diabled button for repair to come back to tmw

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 import { pokemonData } from './pokemon.js';
-import { getRandomPkm } from './utils.js';
+import { getRandomPkm, encounteredPokemon } from './utils.js';
 
 const submitButton = document.getElementById('submit-button');
 const nextButton = document.getElementById('next-button');
 const answerDiv = document.querySelector('#answer');
-const radio = document.querySelectorAll('input');
+
+const radio1 = document.getElementById('input1');
+const radio2 = document.getElementById('input2');
+const radio3 = document.getElementById('input3');
+
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
 
 
-let tries = 0;
 let remainingPkm = pokemonData.slice();
-let pkmMissed = [];
+// let pkmMissed = [];
 // let totalPokemonCaught = 0;
-// let pkmEncountered = pokemonData.slice();
+let pkmEncountered = pokemonData.slice();
 
 let correctAnswer = null;
 
+
+
 function setPage() {
+    
     if (remainingPkm.length === 1) {
         alert('show results');
     }
-        
-    let pkmImageTag1 = document.getElementById('pkm1');
-    let pkmImageTag2 = document.getElementById('pkm2');
-    let pkmImageTag3 = document.getElementById('pkm3');
     
-
+    
     // set event listeners to update state and DOM
-    let randomPkm1 = getRandomPkm(remainingPkm);
+    const randomPkm1 = getRandomPkm(remainingPkm);
     let randomPkm2 = getRandomPkm(remainingPkm);
     let randomPkm3 = getRandomPkm(remainingPkm);
     
     while (randomPkm1.id === randomPkm2.id || randomPkm1.id === randomPkm3.id || randomPkm2.id === randomPkm3.id) {
-        randomPkm1 = getRandomPkm(remainingPkm); 
         randomPkm2 = getRandomPkm(remainingPkm);
         randomPkm3 = getRandomPkm(remainingPkm);
     }
@@ -39,8 +43,10 @@ function setPage() {
     const randomZeroOrOne = Math.random(Math.random());
     const randomOneOrTwo = Math.random(Math.random(randomZeroOrOne));
     
-    console.log(randomPkm1, randomPkm2, randomPkm3);
-
+    // console.log(randomPkm1.pokemon, randomPkm2.pokemon, randomPkm3.pokemon);
+    
+    
+    
     if (randomZeroOrOne === 0) {
         correctAnswer = randomPkm1;
     } else if (randomOneOrTwo === 1) {
@@ -49,102 +55,102 @@ function setPage() {
         correctAnswer = randomPkm3;
     }
     
-    console.log(correctAnswer.name);
+    // console.log(correctAnswer.name);
     
     answerDiv.textContent = correctAnswer.name;
     
+    
+    
     // grabs the array of labels
-    const labels = document.querySelectorAll('label');
-    
-    const firstLabel = labels[0];
-    // const span1 = firstLabel.children[0];
-    const input1 = firstLabel.children[1];
-    // const img1 = firstLabel.children[2];
-    
-    pkmImageTag1.src = randomPkm1.url_image;
-    
-    
-    // does the same to second label
-    const secondLabel = labels[0];
-    // const span2 = secondLabel.children[0];
-    const input2 = secondLabel.children[1];
-    // const img2 = secondLabel.children[2];
-    
-    pkmImageTag2.src = randomPkm2.url_image;
-    
-    
-    // does the same to thrid level
-    const thirdLabel = labels[0];
-    // const span3 = thirdLabel.children[0];
-    const input3 = thirdLabel.children[1];
-    // const img3 = thirdLabel.children[2];
-    
-    pkmImageTag3.src = randomPkm3.url_image;
-    
-    
-    // input1.value = randomPkm1._id;
-    // input2.value = randomPkm2._id;
+    // const labels = document.querySelectorAll('label');
+    // const firstLabel = labels[0];
+    // const secondLabel = labels[1];
+    // const thirdLabel = labels[2];
 
-    input1.disabled = false;
-    input2.disabled = false;
-    input3.disabled = false;
+    // const input1 = firstLabel.children[0];
+    // const input2 = secondLabel.children[0];
+    // const input3 = thirdLabel.children[0];
+    
+    // input1.addEventListener('click', eventHandler);
+    // input2.addEventListener('click', eventHandler);
+    // input3.addEventListener('click', eventHandler);
+
+    // const pkmImageTag1 = firstLabel.children[0];
+    // const pkmImageTag2 = secondLabel.children[1];
+    // const pkmImageTag3 = thirdLabel.children[2];
+
+    // let span1 = firstLabel.children[0];
+    // const span2 = secondLabel.children[1];
+    // const span3 = thirdLabel.children[2];
+
+    encounteredPokemon(pkmEncountered, randomPkm1.id);
+    encounteredPokemon(pkmEncountered, randomPkm2.id);
+    encounteredPokemon(pkmEncountered, randomPkm3.id);
+    
+    radio1.value = randomPkm1.id;
+    radio2.value = randomPkm2.id;
+    radio3.value = randomPkm3.id;
+
+    image1.src = randomPkm1.url_image;
+    image2.src = randomPkm2.url_image;
+    image3.src = randomPkm3.url_image;
+
+    radio1.textContent = randomPkm1.pokemon;
+    radio2.textContent = randomPkm2.pokemon;
+    radio3.textContent = randomPkm3.pokemon;
+
+    // console.log(pkmEncountered, randomPkm1.id);
+    // console.log(randomPkm1.pokemon);
+    // console.log(randomPkm2.pokemon);
+    // console.log(randomPkm3.pokemon);
+    
+
+    radio1.disabled = false;
+    radio2.disabled = false;
+    radio3.disabled = false;
+
     const choiceDiv = document.getElementById('pokemon-choice-container');
     choiceDiv.classList.remove('disabled');
     nextButton.classList.add('hidden');
     
-    // const clickedPkm = document.querySelector('input:checked');
-
+    
     // console.log('clickedPkm', clickedPkm);
-
+    
 }    
 
 
-function eventHandler(e) {
-    tries++;
-    pkmMissed++;
-    
-    const whatTheyClicked = e.target.value;
-    if (whatTheyClicked === correctAnswer.id) {
-        //splice this right answer off our array
-        //import splice function
-        let indexOfPkm;
+submitButton.addEventListener('click', () => {
+    // const playersChoice = document.querySelector('input[type = radio]:checked');
+    // const chosenPkm = playersChoice.value;
 
-        //iterating through all remaining pokemon
-        for (let i = 0; i < remainingPkm.length; i++) {
-            //for each remaining pokemon
-            const pokemon = remainingPkm[i];
-            //if this is the pokemon they picked
-            if (pokemon.id === whatTheyClicked) {
-                //this is the correct index
-                indexOfPkm = i;
-            }
-        }
-        remainingPkm.splice(indexOfPkm, 1);
-        alert('you caught it!');
-    } else {
-        //push the pokemon.id onto the missed pokemon array
-        alert('you missed!');
-        pkmMissed.push(whatTheyClicked);
-    }
+    //Incrementer(itemChose, itemsPickedArray)
+
+    // encounteredPokemon(chosenPkm, id);
+    // pkmMissed++;
     
+    // const whatTheyClicked = event.target.value;
+    // if (whatTheyClicked === correctAnswer.id) {
+    //     let indexOfPkm;
+
+    //     //iterating through all remaining pokemon
+    //     for (let i = 0; i < remainingPkm.length; i++) {
+    //         const pokemon = remainingPkm[i];
+    //         if (pokemon.id === whatTheyClicked) {
+                
+    //             indexOfPkm = i;
+    //         }
+    //     }
+    //     remainingPkm.splice(indexOfPkm, 1);
+    //     alert('you caught it!');
+    // } else {
+    //     //push the pokemon.id onto the missed pokemon array
+    //     alert('you missed!');
+    //     pkmMissed.push(whatTheyClicked);
+    // }
    
-}
-submitButton.addEventListener('click', setPage);
-
-setPage();
-
-radio.forEach((radioTag) => {
-    radioTag.addEventListener('click', (event) => {
-        tries++;
-        let whatTheyClicked = itemCopy.getRandomPkm(event.target.value);
-        if (event.target.value === whatTheyClicked.id) {
-            whatTheyClicked.clicks = whatTheyClicked.clicks + 1;
-        }
-        itemCopy.save('session');
-        getRandomPkm();
-    });
-
-setPage();
-
 });
+
+
+setPage();
+
 
--- utils.js
+++ utils.js
@@ -1,6 +1,74 @@
+// import { pokemonData } from './pokemon.js';
+
 export function getRandomPkm(pkmArray) {
     const randomIndex = Math.floor(Math.random() * pkmArray.length);
 
     return pkmArray[randomIndex];
 }
+
+
+export function findById(pkmArray, pkmId) {
+    let matchItem = null;
+    const pkmNumber = Number(pkmId);
+
+    for (let i = 0; i < pkmArray.length; i++) {
+
+        if (pkmNumber === pkmArray[i].id) {
+            matchItem = pkmArray[i];
+        }
+    }
+    return matchItem;
+}
+
+
+export function encounteredPokemon(pkmEncountered, id) {
+    let encountered = findById(pkmEncountered, id);
+
+    if (encountered) {
+        encountered.encounters++;
+
+    } else {
+        const newEncounter = {
+            id: id,
+            encounters: 1,
+            caught: 0
+        };
+
+        pkmEncountered.push(newEncounter);
+    }
+}
+
+
+export function caughtPokemon(pkmEncountered, id) {
+    let caught = findById(pkmEncountered, id);
+
+    if (caught) {
+        caught.caught++;
+
+    } else {
+        const newCaught = {
+            id: id,
+            encounters: 1,
+            caught: 1
+        };
+        pkmEncountered.push(newCaught);
+    }
+}
+
+
+//all encounters + captures per user session: 0,
+
+// export function incrementor(pkmArray, id) {
+//     function addInitialitem(pkmArray, id) {
+//         const initialItem = {
+//             id: id,
+//             encounters: 0,
+//             caught: 0
+//         };
+
+//         pkmArray.push(initialItem);
+//     }
+
+//     let itemSeen = findById(pkmArray, id)
+// }
 
